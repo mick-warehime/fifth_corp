c71e091acf95983e6b26230ea66e10d44a32c359
Miiick <mickwarehime@gmail.com>
2018-08-17 18:17:04 -0700

    add tests

--- src/tests/test_test.py
+++ src/tests/test_test.py
@@ -1,3 +1,9 @@
-def test_hello_world():
-    hello = 'hello_world'
-    assert 'hello_world' == hello
+import controller
+from PyQt5.QtWidgets import QMainWindow
+
+def combat_controller():
+    m = QMainWindow()
+    return controller.CombatController(m)
+
+def test_hello_world(qtbot):
+    assert combat_controller() is not None
